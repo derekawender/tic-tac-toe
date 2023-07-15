bac72658cd2f38e7f5c19b88d981badb694e4024
Derek Awender <derekawender@berkeley.edu>
2023-07-15 16:48:31 -0700

    added dockerfile and changed the name of Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main (String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         Game game = new Game();
